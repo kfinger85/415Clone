704ccf7f85232c3db880ac6f90b375673b76e92d
Kevin Finger <kfinger85@gmail.com>
2023-05-26 09:57:39 -0600

    Annotation add

--- server/src/main/java/edu/colostate/cs415/repositories/CompanyRepository.java
+++ server/src/main/java/edu/colostate/cs415/repositories/CompanyRepository.java
@@ -2,7 +2,10 @@ package edu.colostate.cs415.repositories;
 
 import edu.colostate.cs415.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 Company findByName(String name);
 }
