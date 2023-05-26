032b9377cfacd04e94f08682f32d5cb3d73851ea
Kevin Finger <kfinger85@gmail.com>
2023-05-26 10:05:31 -0600

    React textAlign

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,16 @@
 import { pageStyle } from '../utils/styles'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import LoginPage from './login';
 
 const Home = () => {
     return (
         <div style={pageStyle}>
-            <div style={{ 'text-align': 'center' }}>
+            <div style={{ 'textAlign': 'center' }}>
                 <h1>Company Management</h1>
                 <br />
                 <h2>CS415 - Colorado State University</h2>
                 <h2>Spring 2023</h2>
+                <LoginPage />
             </div>
         </div>
     )
