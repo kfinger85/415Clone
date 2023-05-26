d3e6f5b0932f032042c06c4067603fe29a67388a
Kevin Finger <kfinger85@gmail.com>
2023-05-26 09:57:49 -0600

    Qualification findByName

--- server/src/main/java/edu/colostate/cs415/repositories/QualificationRepository.java
+++ server/src/main/java/edu/colostate/cs415/repositories/QualificationRepository.java
@@ -4,4 +4,5 @@ import edu.colostate.cs415.model.Qualification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QualificationRepository extends JpaRepository<Qualification, Long> {
+    Qualification findByName(String name);
 }
