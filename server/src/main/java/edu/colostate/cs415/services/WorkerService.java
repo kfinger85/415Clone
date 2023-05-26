7b3f4637d400b7967a5c96601ad40d11ff8984e1
Kevin Finger <kfinger85@gmail.com>
2023-05-26 10:01:27 -0600

    WorkerService - getters

--- server/src/main/java/edu/colostate/cs415/services/WorkerService.java
+++ server/src/main/java/edu/colostate/cs415/services/WorkerService.java
@@ -2,13 +2,18 @@ package edu.colostate.cs415.services;
 
 import edu.colostate.cs415.repositories.WorkerRepository;
 import edu.colostate.cs415.model.Worker;
+import edu.colostate.cs415.dto.WorkerDTO;
 import edu.colostate.cs415.model.Qualification;
 import edu.colostate.cs415.repositories.QualificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class WorkerService {
@@ -17,13 +22,38 @@ public class WorkerService {
     private WorkerRepository workerRepository;
 
     @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
     private QualificationRepository qualificationRepository;
 
-    @Transactional
-    public Worker createWorker(String name, Set<Qualification> qualifications, double salary) {
+
+    public Worker createWorker(String name, String username, Set<Qualification> qualifications, double salary, String password) {
+        String hashedPassword = passwordEncoder.encode(password);
         Worker worker = new Worker(name, qualifications, salary);
+        worker.setPassword(hashedPassword);
+        worker.setUsername(username);
         return workerRepository.save(worker);
     }
+
+
+    @Transactional(readOnly = true)
+    public List<WorkerDTO> getAllWorkers() {
+        List<WorkerDTO> workers = workerRepository.findAll().stream()
+                                                   .map(Worker::toDTO)
+                                                   .collect(Collectors.toList());
+        return workers;
+    }
+
+    @Transactional(readOnly = true)
+    public WorkerDTO getWorker(String name) {
+        Worker worker = workerRepository.findByName(name);
+        if (worker == null) {
+            throw new RuntimeException("Worker not found");
+        }
+        return worker.toDTO();
+    }
+
 
     @Transactional
     public void assignQualificationToWorker(Long workerId, Long qualificationId) {
